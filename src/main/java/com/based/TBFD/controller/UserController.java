3d8b62452f60a79389313c31fe98b29340959516
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-01-05 11:23:34 -0600

    Added Envs

---
+++ src/main/java/com/based/TBFD/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.based.TBFD.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(exposedHeaders = {"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"})
+@RequestMapping("/api/v1/users/")
+@RestController
+public class UserController {
+
+//    @PostMapping
+//    public ResponseEntity<>
+}
