2af2a364efb612208b1d5933df7b60a2ddc093a2
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-01-05 10:05:03 -0600

    Project

---
+++ src/main/java/com/based/TBFD/TbfdApplication.java
@@ -0,0 +1,13 @@
+package com.based.TBFD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbfdApplication.class, args);
+	}
+
+}
